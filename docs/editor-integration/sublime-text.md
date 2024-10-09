1997c32002a26fb434adf140780dc27ef6d66376
aspizu <aspizu@protonmail.com>
2024-10-09 19:02:55 +0530

    Add build task to vscode extension.

---
+++ docs/editor-integration/sublime-text.md
@@ -0,0 +1,8 @@
+## Sublime Text
+
+The Sublime Text extension provides syntax highlighting.
+
+### Install from source
+
+in Sublime Text, click `Preferences > Browse Packages...` to open up the Packages folder,
+and then copy `editors/sublime/goboscript.sublime-syntax` into the Packages folder
--- editors/code/.eslintrc.js
+++ editors/code/.eslintrc.js
@@ -1,45 +1,45 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: ["prettier"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname,
-        sourceType: "module",
-    },
-    plugins: ["@typescript-eslint"],
-    rules: {
-        camelcase: ["error"],
-        eqeqeq: ["error", "always", {null: "ignore"}],
-        curly: ["error", "multi-line"],
-        "no-console": ["error", {allow: ["warn", "error"]}],
-        "prefer-const": "error",
-        "@typescript-eslint/member-delimiter-style": [
-            "error",
-            {
-                multiline: {
-                    delimiter: "semi",
-                    requireLast: true,
-                },
-                singleline: {
-                    delimiter: "semi",
-                    requireLast: false,
-                },
-            },
-        ],
-        "@typescript-eslint/no-unnecessary-type-assertion": "error",
-        "@typescript-eslint/no-floating-promises": "error",
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    camelcase: ["error"],
+    eqeqeq: ["error", "always", { null: "ignore" }],
+    curly: ["error", "multi-line"],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "prefer-const": "error",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: "semi",
+          requireLast: false,
+        },
+      },
+    ],
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "@typescript-eslint/no-floating-promises": "error",
 
-        "@typescript-eslint/consistent-type-imports": [
-            "error",
-            {
-                prefer: "type-imports",
-                fixStyle: "inline-type-imports",
-            },
-        ],
-        "@typescript-eslint/no-import-type-side-effects": "error",
-    },
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-import-type-side-effects": "error",
+  },
 }
