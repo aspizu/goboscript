ab4accb774696fe0f9e46b784801c8025cded27a
aspizu <aspizu@protonmail.com>
2025-05-20 02:05:16 +0530

    feat: add default value to struct field syntax

--- src/ast/struct_field.rs
+++ src/ast/struct_field.rs
@@ -1,10 +1,12 @@
 use logos::Span;
 
+use super::Value;
 use crate::misc::SmolStr;
 
 #[derive(Debug)]
 pub struct StructField {
     pub name: SmolStr,
     pub span: Span,
+    pub default: Option<(Value, Span)>,
     pub is_used: bool,
 }
