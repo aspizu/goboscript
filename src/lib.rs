79504078b3cbbeea73b5875c65dce43396b7743c
Priyanshu Dangare <aspizu@protonmail.com>
2026-02-09 03:34:21 +0530

    fix: remove compilation of reqwest for wasm targets

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,34 @@ pub mod lexer;
 pub mod misc;
 pub mod parser;
 pub mod pre_processor;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod standard_library;
+#[cfg(target_arch = "wasm32")]
+pub mod standard_library {
+    use std::path::Path;
+
+    use semver::Version;
+
+    pub struct StandardLibrary {
+        pub version: Version,
+        pub path: std::path::PathBuf,
+    }
+
+    pub fn standard_library_from_latest(cache_path: &Path) -> anyhow::Result<StandardLibrary> {
+        unreachable!()
+    }
+
+    pub fn fetch_standard_library(stdlib: &StandardLibrary) -> anyhow::Result<()> {
+        unreachable!()
+    }
+
+    pub fn new_standard_library(version: Version, cache_path: &Path) -> StandardLibrary {
+        StandardLibrary {
+            path: cache_path.join(format!("v{}", version)),
+            version,
+        }
+    }
+}
 pub mod translation_unit;
 pub mod vfs;
 pub mod visitor;
