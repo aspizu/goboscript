ce3a5951a1277369a6b952a330de0d733c6d9848
Priyanshu Dangare <aspizu@protonmail.com>
2026-03-14 04:38:06 +0530

    fix: deployment dir

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,4 +4,4 @@ cd "$(realpath -m "$0/../..")"
 [[ -z "$MKDOCS_GIT_COMMITTERS_APIKEY" ]] && export MKDOCS_GIT_COMMITTERS_APIKEY="$(gh auth token)"
 export NO_MKDOCS_2_WARNING=1
 uv sync
-uv run mkdocs build --strict -d "$(pwd)/site/"
+uv run mkdocs build --strict -d "$(pwd)/docs/site/"
