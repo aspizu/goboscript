511dccd7a40357cf60c85e7432d47e73ca7930f6
Priyanshu Dangare <aspizu@protonmail.com>
2026-03-14 03:55:14 +0530

    docs: remove social plugin

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 set -e
-cd "$(dirname "$(dirname "$(realpath "$0")")")"
+cd "$(realpath -m "$0/../..")"
+[[ -z "$MKDOCS_GIT_COMMITTERS_APIKEY" ]] && export MKDOCS_GIT_COMMITTERS_APIKEY="$(gh auth token)"
 export NO_MKDOCS_2_WARNING=1
 uv sync
 uv run mkdocs build --strict
